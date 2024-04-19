d632e0b2ffa58c166d33791bb6e4fa5ffd9cac4d
JoeMcCleery <mccleery.joseph@gmail.com>
2024-04-19 15:35:46 +0800

    Add header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="sticky top-0 max-h-screen border-b border-emerald-800 bg-emerald-700 p-4 text-emerald-50 md:border-b-0 md:border-r">
+      <h1 className="bold text-3xl">Joe McCleery</h1>
+    </header>
+  );
+}
+
+export default Header;
