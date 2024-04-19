ae182346bb2a52afe75ebc06c7012948c7f6606f
JoeMcCleery <mccleery.joseph@gmail.com>
2024-04-19 15:35:56 +0800

    Add footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="flex items-center justify-center border-t border-emerald-800 bg-emerald-700 p-4 text-emerald-50">
+      <p>Footer</p>
+    </footer>
+  );
+}
+
+export default Footer;
