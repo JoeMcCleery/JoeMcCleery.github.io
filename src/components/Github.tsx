457d51b1d746b7f4d3cfda8402946c871ebd75d1
JoeMcCleery <mccleery.joseph@gmail.com>
2024-04-20 00:34:36 +0800

    Bugfix username prop wrong

--- src/components/Github.tsx
+++ src/components/Github.tsx
@@ -1,14 +1,14 @@
 import UserRepos from "components/UserRepos";
 
 function Github() {
-  const users = ["JoeMcCleery", "FearIndigo"];
+  const usernames = ["JoeMcCleery", "FearIndigo"];
 
   return (
     <section id="github" className="p-4">
       <h2 className="mb-4 text-2xl font-bold">Github</h2>
       <div className="space-y-4">
-        {users.map((user, idx) => (
-          <UserRepos key={idx} user={user} />
+        {usernames.map((username, idx) => (
+          <UserRepos key={idx} username={username} />
         ))}
       </div>
     </section>
