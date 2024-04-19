b740d7ea54eecc19eb7f10ac1436eeeb1646ff10
JoeMcCleery <mccleery.joseph@gmail.com>
2024-04-19 23:02:06 +0800

    Add FancyToggle component

---
+++ src/components/FancyToggle.tsx
@@ -0,0 +1,16 @@
+interface FancyToggleProps {
+  isOn: boolean;
+}
+
+function FancyToggle({ isOn }: FancyToggleProps) {
+  return (
+    <div className="relative size-full">
+      <div className="absolute top-[50%] h-1 w-full -translate-y-[50%] bg-emerald-50"></div>
+      <div
+        className={`ease absolute left-[50%] h-full w-1 -translate-x-[50%] bg-emerald-50 transition-transform ${isOn ? "rotate-90" : "rotate-0"}`}
+      ></div>
+    </div>
+  );
+}
+
+export default FancyToggle;
