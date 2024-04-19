8e639fab6bc36a84412f63eaee28cf0415ec1b76
JoeMcCleery <mccleery.joseph@gmail.com>
2024-04-19 23:02:50 +0800

    Update MobileNavigation component

--- src/components/MobileNavigation.tsx
+++ src/components/MobileNavigation.tsx
@@ -1,20 +1,41 @@
 import { useState } from "react";
 import Navigation from "components/Navigation";
+import FancyToggle from "./FancyToggle";
 
 function MobileNavigation() {
   const [navOpen, setNavOpen] = useState(false);
 
   return (
-    <div className="flex h-full justify-end md:hidden">
+    <div className="flex justify-end md:hidden">
       <button
-        className="aspect-square h-full border-l border-emerald-800 bg-emerald-600 text-xl hover:bg-emerald-700"
+        className="box-content aspect-square border-l border-emerald-800 bg-emerald-600 p-4 "
         onClick={() => {
           setNavOpen(!navOpen);
         }}
       >
-        {navOpen ? "-" : "+"}
+        <FancyToggle isOn={navOpen} />
       </button>
-      {navOpen && <Navigation />}
+
+      <div className="absolute left-0 top-full w-full">
+        <div
+          className={`grid transition-[grid-template-rows] ${navOpen ? "grid-rows-[1fr]" : "grid-rows-[0fr]"}`}
+        >
+          <div className="overflow-hidden">
+            <Navigation
+              onSelection={() => {
+                setNavOpen(false);
+              }}
+            />
+          </div>
+        </div>
+      </div>
+
+      <button
+        className={`absolute top-full -z-10 w-full bg-emerald-800/50 backdrop-blur-sm transition-opacity ${navOpen ? "h-[calc(100vh-100%)] opacity-100" : "opacity-0"}`}
+        onClick={() => {
+          setNavOpen(false);
+        }}
+      ></button>
     </div>
   );
 }
