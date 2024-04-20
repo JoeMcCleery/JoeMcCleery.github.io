6a34b381e03cd13ea1ca97bc44067abe637c704b
JoeMcCleery <mccleery.joseph@gmail.com>
2024-04-20 09:43:49 +0800

    add useFetchHook

--- src/components/UserRepos.tsx
+++ src/components/UserRepos.tsx
@@ -1,34 +1,31 @@
-import { useEffect, useState } from "react";
 import Repo from "./Repo";
 import { Repositories } from "types/repositories";
+import { useFetch } from "hooks/useFetch";
 
 interface RepositoriesProps {
   username: string;
 }
 
 function UserRepos({ username }: RepositoriesProps) {
-  const [data, setData] = useState<Repositories>([]);
-
-  useEffect(() => {
-    fetch(`https://api.github.com/users/${username}/repos`)
-      .then((response) => response.json())
-      .then((result: Repositories) => {
-        setData(result);
-        console.log(result);
-      })
-      .catch((error: unknown) => {
-        console.log(error);
-      });
-  }, [username]);
+  const [data, isLoading] = useFetch<Repositories>(
+    `https://api.github.com/users/${username}/repos`,
+    [],
+  );
 
   return (
     <div>
-      <h3 className="mb-2 text-xl font-bold">{username}</h3>
-      <div className="space-y-1">
-        {data.map((repoData, idx) => (
-          <Repo key={idx} data={repoData} />
-        ))}
-      </div>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <h3 className="mb-2 text-xl font-bold">{username}</h3>
+          <div className="space-y-1">
+            {data.map((repoData, idx) => (
+              <Repo key={idx} data={repoData} />
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 }
---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+export function useFetch<T>(url: string, defaultData: T) {
+  const [data, setData] = useState<T>(defaultData);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<unknown>();
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(url)
+      .then((response) => response.json())
+      .then((result: T) => {
+        setData(result);
+      })
+      .catch((error: unknown) => {
+        setError(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [url]);
+
+  return [data, isLoading, error] as const;
+}
