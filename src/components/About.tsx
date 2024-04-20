190cdfbe696d4675c3e86349c4c17e1a836c59de
JoeMcCleery <mccleery.joseph@gmail.com>
2024-04-20 15:20:17 +0800

    Better spacing/margins

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,8 +1,8 @@
 function About() {
   return (
     <section id="about" className="p-8">
-      <h2 className="mb-4 text-2xl font-bold">About Me</h2>
-      <p>Software developer from Perth, Western Australia</p>
+      <h2 className="mb-8 text-2xl font-bold">About Me</h2>
+      <p>Software developer from Perth, Western Australia.</p>
     </section>
   );
 }
--- src/components/Github.tsx
+++ src/components/Github.tsx
@@ -5,8 +5,8 @@ function Github() {
 
   return (
     <section id="github" className="border-t border-emerald-800 p-8">
-      <h2 className="mb-4 text-2xl font-bold">Github</h2>
-      <div className="space-y-4">
+      <h2 className="mb-8 text-2xl font-bold">Github</h2>
+      <div className="space-y-8">
         {usernames.map((username, idx) => (
           <UserRepos key={idx} username={username} />
         ))}
