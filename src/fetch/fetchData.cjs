478502a8ea9565517ca433b0b0c3a961fe6e761a
JoeMcCleery <mccleery.joseph@gmail.com>
2024-06-18 15:26:17 +0800

    fetch user info workflow

--- src/fetch/fetchData.cjs
+++ src/fetch/fetchData.cjs
@@ -1,7 +1,11 @@
 const usernames = ["JoeMcCleery", "FearIndigo"];
 
-function fetchData({ github, context, core }) {
-  console.log("test");
+function fetchData({ github, context, core, io }) {
+  const users = [];
+  usernames.forEach((username) => {
+    const user = await github.rest.users.getByUsername({ username });
+    console.log(user);
+  });
 }
 
 module.exports = fetchData;
