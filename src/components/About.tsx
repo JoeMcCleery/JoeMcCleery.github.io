14bf8bbbcf5ac8c018b9fac3e636c8881e365d03
JoeMcCleery <mccleery.joseph@gmail.com>
2024-08-28 14:12:31 +0800

    Updated about section content

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -9,7 +9,19 @@ function About() {
         <h2 className="text-2xl font-bold">About Me</h2>
       </div>
 
-      <p className="mb-4">Software developer from Perth, Western Australia.</p>
+      <div className="mb-4 space-y-4">
+        <p className="text-lg">
+          Dedicated Software Developer from Perth Western Australia with a
+          passion for creativity, collaboration, and continuous learning.
+        </p>{" "}
+        <p>
+          Skilled in C#, Typescript, HTML, and CSS. Experienced in frontend
+          frameworks (React.js, Vue.js) and game engines (Unity, Godot). Proven
+          ability to develop high-quality, cross-platform applications while
+          working effectively in team environments.
+        </p>
+        <p>Eager to explore the application of AI in software development.</p>
+      </div>
 
       <ContactLinks />
     </section>
